0e3927be88a68f301b39ea653a79f4a9fd313293
Gerwin Brunner <gerwin.brunner@vilango.com>
2019-07-04 11:38:01 +0200

    reverted fix for num values

--- packages/uniforms-polaris/src/NumField.js
+++ packages/uniforms-polaris/src/NumField.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React /*, {Component} */ from 'react';
 import {TextField} from '@shopify/polaris';
 
 import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
-const noneIfNaN = x => (isNaN(x) ? undefined : x);
+// const noneIfNaN = x => (isNaN(x) ? undefined : x);
 
 const Num_ = ({
   decimal,
@@ -37,34 +37,34 @@ const Num_ = ({
   />
 );
 
-// NOTE: React < 16 workaround. Make it optional?
-class Num extends Component {
-  constructor() {
-    super(...arguments);
+// // NOTE: React < 16 workaround. Make it optional?
+// class Num extends Component {
+//   constructor() {
+//     super(...arguments);
 
-    this.state = {value: '' + this.props.value};
+//     this.state = {value: '' + this.props.value};
 
-    this.onChange = this.onChange.bind(this);
-  }
+//     this.onChange = this.onChange.bind(this);
+//   }
 
-  componentWillReceiveProps({decimal, value}) {
-    const parse = decimal ? parseFloat : parseInt;
+//   componentWillReceiveProps({decimal, value}) {
+//     const parse = decimal ? parseFloat : parseInt;
 
-    if (noneIfNaN(parse(value)) !== noneIfNaN(parse(this.state.value.replace(/[.,]+$/, '')))) {
-      this.setState({value: value === undefined || value === '' ? '' : '' + value});
-    }
-  }
+//     if (noneIfNaN(parse(value)) !== noneIfNaN(parse(this.state.value.replace(/[.,]+$/, '')))) {
+//       this.setState({value: value === undefined || value === '' ? '' : '' + value});
+//     }
+//   }
 
-  onChange(value) {
-    const change = value.replace(/[^\d.,-]/g, '');
+//   onChange(value) {
+//     const change = value.replace(/[^\d.,-]/g, '');
 
-    this.setState({value: change});
-    this.props.onChange(noneIfNaN((this.props.decimal ? parseFloat : parseInt)(change)));
-  }
+//     this.setState({value: change});
+//     this.props.onChange(noneIfNaN((this.props.decimal ? parseFloat : parseInt)(change)));
+//   }
 
-  render() {
-    return Num_({...this.props, onChange: this.onChange, value: this.state.value});
-  }
-}
+//   render() {
+//     return Num_({...this.props, onChange: this.onChange, value: this.state.value});
+//   }
+// }
 
-export default connectField(Num);
+export default connectField(Num_);
